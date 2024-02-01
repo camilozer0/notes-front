f4246e07cb98a6f1695fa6b34940a88f2e264ef8
Camilo Mazorra <mazorra006@gmail.com>
2024-02-01 15:55:08 -0500

    navbar listo, edittando sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { OptionComponent } from "../option/option.component";
 
 @Component({
-  selector: 'shared-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+    selector: 'shared-sidebar',
+    standalone: true,
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.css',
+    imports: [OptionComponent]
 })
 export class SidebarComponent {
 

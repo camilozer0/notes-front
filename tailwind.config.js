b2b5d63c9088d2c54637481c1a957d78f941e08d
Camilo Mazorra <mazorra006@gmail.com>
2024-02-01 12:08:18 -0500

    configurando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      softwhite: '#D9D9D9',
+      softgray: '#848CA3',
+      mediumgray: '#323746',
+      darkgray: '#181B25',
+    }
+  },
+  plugins: [],
+}
+

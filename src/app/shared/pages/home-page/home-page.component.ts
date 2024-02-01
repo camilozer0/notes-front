c46ce72a1f78ba5af24bf2acf3ed24ef8fdd023f
Camilo Mazorra <mazorra006@gmail.com>
2024-02-01 14:37:43 -0500

    editando  la navbar

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 
 @Component({
     selector: 'shared-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [NavbarComponent]
+    imports: [NavbarComponent, SidebarComponent]
 })
 export class HomePageComponent {
 

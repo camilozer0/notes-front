3b08e6ff73bde2cc39fab044fe5e06b895202ee8
Camilo Mazorra <mazorra006@gmail.com>
2024-02-05 12:22:18 -0500

    editando todos que se muestran

--- src/app/todos/pages/upcoming/upcoming.component.ts
+++ src/app/todos/pages/upcoming/upcoming.component.ts
@@ -9,7 +9,13 @@ import { TodosTableComponent } from '../../components/todos-table/todos-table.co
     CommonModule,
     TodosTableComponent
   ],
-  template: `<component-todos-table></component-todos-table>`,
+  template: `
+  <div class="bg-softgray m-6 rounded-sm
+  ">
+    <component-todos-table></component-todos-table>
+  </div>
+
+  `,
   styleUrl: './upcoming.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })

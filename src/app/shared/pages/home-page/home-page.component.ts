f5bcd4144222d23474083aa5cc7051f97b4e0e66
Camilo Mazorra <mazorra006@gmail.com>
2024-02-07 22:34:45 -0500

    editando home y todos

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
     selector: 'shared-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [NavbarComponent]
+    imports: [
+      NavbarComponent,
+      FooterComponent,
+    ]
 })
 export class HomePageComponent {
 
